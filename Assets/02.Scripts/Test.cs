33d93c80a03b5ef11026e2b926b9b1060fadf287
정종혁 <whdgur0068@gmail.com>
2025-12-08 12:54:17 +0900

    Chore : 폴더 구분

---
+++ Assets/02.Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // 한글 테스트
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

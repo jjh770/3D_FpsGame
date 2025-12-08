e34731cc7162336f5055d77b273ab3a1f3897427
정종혁 <whdgur0068@gmail.com>
2025-12-08 16:02:42 +0900

    Add : 카메라 회전, 플레이어 이동, 플레이어 회전

---
+++ Assets/02.Scripts/Player/PlayerRotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    public float RotationSpeed = 200f; // 0 ~ 360;
+
+    private float _accumulationX = 0;
+
+    private void Update()
+    {
+        if (!Input.GetMouseButton(1))
+        {
+            return;
+        }
+
+        float mouseX = Input.GetAxis("Mouse X");
+        _accumulationX += mouseX * RotationSpeed * Time.deltaTime;
+
+        transform.eulerAngles = new Vector3(0, _accumulationX);
+    }
+}

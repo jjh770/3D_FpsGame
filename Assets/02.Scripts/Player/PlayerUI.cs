16a1e099dd00fd83487df297ccbee83a7d445c68
jjh770 <whdgur0068@gmail.com>
2025-12-08 23:23:19 +0900

    Add : 실습과제 진행
    스태미나 / 빨리 뛰기 구현
    - 스태미나는 자동으로 회복된다.
    - Shift를 누르고 있으면 스태미나가 소모되며, 빨리 뛰기
    - 왼쪽 하단에 Slider를 이용해 스태미너 UI를 표시
    
    2단 점프 구현
    - 그냥 점프는 스태미나 소모하지 않음
    - 점프 한 상태에서 일정 스태미나를 소모하여 2단 점프 가능
    - 스태미너 UI는 체력도 넣어두기

---
+++ Assets/02.Scripts/Player/PlayerUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUI : MonoBehaviour
+{
+    [Header("UI References")]
+    [SerializeField] private Slider _healthSlider;
+    [SerializeField] private Slider _staminaSlider;
+
+    private PlayerMove _playerMove;
+    private float _currentHealth;
+
+    private void Start()
+    {
+        _playerMove = FindObjectOfType<PlayerMove>();
+
+        if (_staminaSlider != null)
+        {
+            _staminaSlider.maxValue = _playerMove.MaxStamina;
+            _staminaSlider.value = _playerMove.CurrentStamina;
+        }
+    }
+
+    private void Update()
+    {
+        UpdateStaminaUI();
+    }
+
+    private void UpdateStaminaUI()
+    {
+        if (_staminaSlider != null && _playerMove != null)
+        {
+            _staminaSlider.value = _playerMove.CurrentStamina;
+        }
+    }
+}

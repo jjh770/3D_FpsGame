5d750a147666b751d87805a710ad5bc9b61e52ab
정종혁 <whdgur0068@gmail.com>
2025-12-10 16:34:23 +0900

    Study : 코루틴 학습

---
+++ Assets/02.Scripts/Grammer/CoroutineExample_1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineExample_1 : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Test1();
+            // 코루틴 : 협력(Co) + 루틴(Routine)의 합성어 : 협력 동작
+            StartCoroutine(Test2());
+            // 1초 시간이 걸린다. (렉, 프리징, 블로킹)
+            // -> 코드를 '동기'적으로 실행하는게 아니라 '비동기' 적으로 실행하고 싶다.
+            //   동기 -> 이전 코드가 실행 완료된 다음에 그 다음 코드를 실행하는 것.
+            // 비동기 -> 이전 코드가 실행 완료 여부와 상관 없이 (다른) 코드를 실행하는 것.
+            // 유니티에서는 '비동기' 방식을 지원하기 위해 "코루틴"이라는 기능을 제공한다.
+            Test3();
+        }
+    }
+
+    private void Test1()
+    {
+        Debug.Log("Test1");
+    }
+
+    private IEnumerator Test2()
+    {
+        int sum = 0;
+        for (int i = 0; i < 100000; i++)
+        {
+            for (int j = 0; j < 10000; j++)
+            {
+                yield return null;
+                sum += (i * j);
+            }
+        }
+        Debug.Log(sum);
+    }
+
+    private void Test3()
+    {
+        Debug.Log("Test3");
+    }
+
+}
---
+++ Assets/02.Scripts/Grammer/CoroutineExample_2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineExample_2 : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+        }
+    }
+
+    private IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(1.6f);
+        // Reload()
+    }
+
+}

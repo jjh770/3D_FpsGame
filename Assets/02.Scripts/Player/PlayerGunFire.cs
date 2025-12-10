e73f14a4a55c983006e564f540c82b083ef4b257
정종혁 <whdgur0068@gmail.com>
2025-12-10 14:47:04 +0900

    Fix : 총 발사, 발사처리 역할 구분

--- Assets/02.Scripts/Player/PlayerGunFire.cs
+++ Assets/02.Scripts/Player/PlayerGunFire.cs
@@ -1,105 +1,22 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerGunFire : MonoBehaviour
 {
     // 마우스의 왼쪽 버튼을 누르면 바라보는 방향으로 총을 발사하고 싶다. (총을 발사하고 싶다)
-    [SerializeField] private Transform _fireTransform;
-    [SerializeField] private ParticleSystem _hitEffectVFX;
-    [SerializeField] private WeaponStats _weaponStats;
-
-    private float _reloadTime = 1.6f;
-    private bool _isReloading = false;
-
-    private float _coolDown = 0.1f;
-    private float _timer = 1f;
+    [SerializeField] private WeaponController _weaponController;
 
     private void Update()
     {
-        _timer += Time.deltaTime;
+        if (_weaponController == null) return;
 
         // 1. 마우스 왼쪽 버튼이 눌린다면
         if (Input.GetMouseButton(0))
         {
-            if (_timer < _coolDown || _isReloading) return;
-
-            _timer = 0f;
-
-            if (!TryShoot()) return;
-
-            // 2. Ray를 생성하고 [발사할 위치], [방향], [거리(생략가능)]를 설정한다 (쏜다.)
-            Ray ray = new Ray(_fireTransform.position, Camera.main.transform.forward);
-
-            // 3. RayCastHit(충돌한 대상의 정보)를 저장할 변수를 생성한다.
-            RaycastHit hitInfo = new RaycastHit();
-
-            // 4. [발사하고], [충돌했다면] -> 맞았는가 안맞았는가 Bool 제공
-            bool isHit = Physics.Raycast(ray, out hitInfo);
-            if (isHit)
-            {
-                // 5. 충돌했다면 피격 이펙트 표시
-                Debug.Log(hitInfo.transform.name);
-
-                // 파티클생성과 플레이 방식
-                // 1. Instantiate 방식 (+ 풀링) -> 새로 생성(메모리, CPU 측면으로는 좋음)
-                // -> 한 화면에 여러가지 수정 후 여러 개 그릴 경우
-
-                // ParticleSystem hitEffect = Instantiate(_hitEffectPrefab, hitInfo.point, Quaternion.identity);
-
-
-                // 2. 하나를 캐싱해두고 Play    -> 단점 : 재실행이므로 기존 Play 중인 파티클 삭제 (Hierachy에 두기)
-                // 인스펙터 설정 그대로 그릴 경우 (내부적으로는 Emit과 동일함)
-
-                // hitInfo에는 point이라는 정보를 제공함 -> point는 부딫힌 위치 반환
-                _hitEffectVFX.transform.position = hitInfo.point;
-                // hitInfo에는 normal이라는 정보를 제공함 -> normal은 법선벡터(부딫히면 튕겨져 나오는 방향)
-                _hitEffectVFX.transform.forward = hitInfo.normal;
-                _hitEffectVFX.Play();
-
-                // 3. 하나를 캐싱해두고 Emit
-                // 인스펙터 설정을 수정 후 그릴 경우 (transform을 제외한 다른 설정을 바꾸고 싶을 때 emitParams 세팅)
-                // 파티클 시스템 local vs world
-                // 빌보드란?
-            }
+            _weaponController.TryShoot();
         }
-        if (Input.GetKeyDown(KeyCode.R) && !_isReloading)
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            StartCoroutine(ReloadBullet());
+            _weaponController.TryReload();
         }
-    }
-
-    private bool TryShoot()
-    {
-        if (_weaponStats.BulletCount.IsEmpty())
-        {
-            StartCoroutine(ReloadBullet());
-            return false;
-        }
-        else
-        {
-            _weaponStats.BulletCount.TryConsume();
-            return true;
-        }
-
-    }
-    private IEnumerator ReloadBullet()
-    {
-        if (_isReloading || _weaponStats.BulletClipCount.IsEmpty() || _weaponStats.BulletCount.IsFull()) yield break;
-
-        _isReloading = true;
-
-        yield return new WaitForSeconds(_reloadTime);
-
-        int currentBullet = _weaponStats.BulletCount.CurrentCount;
-        int maxBullet = _weaponStats.BulletCount.MaxCount;
-        int reserveBullet = _weaponStats.BulletClipCount.CurrentCount;
-
-        int bulletNeeded = maxBullet - currentBullet;
-        int bulletToReload = Mathf.Min(bulletNeeded, reserveBullet);
-
-        _weaponStats.BulletCount.Add(bulletToReload);
-        _weaponStats.BulletClipCount.TryConsume(bulletToReload);
-
-        _isReloading = false;
     }
 }
--- Assets/02.Scripts/Weapon/WeaponStats.cs
+++ Assets/02.Scripts/Weapon/WeaponStats.cs
@@ -1,25 +1,25 @@
 ﻿using UnityEngine;
 
-// 플레이어의 '스탯'을 관리하는 컴포넌트
+// 무기의 '스탯'을 관리하는 컴포넌트
 public class WeaponStats : MonoBehaviour
 {
     [SerializeField] private ResourceStat _bullet;
     [SerializeField] private ResourceStat _bulletClipCount;
     [SerializeField] private ResourceStat _bombCount;
+    [SerializeField] private float _reloadTime = 1.6f;
+    [SerializeField] private float _coolTime = 0.1f;
 
     public ResourceStat BulletCount => _bullet;
     public ResourceStat BulletClipCount => _bulletClipCount;
     public ResourceStat BombCount => _bombCount;
+    public float ReloadTime => _reloadTime;
+    public float CoolTime => _coolTime;
+    public bool IsReloading { get; set; }
 
     private void Start()
     {
         _bullet.Initialize();
         _bulletClipCount.Initialize();
         _bombCount.Initialize();
-    }
-
-    private void Update()
-    {
-        float deltaTime = Time.deltaTime;
     }
 }

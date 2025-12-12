b8a6db96e681fc7ec7e4cb3cdac1efa174d33889
정종혁 <whdgur0068@gmail.com>
2025-12-12 13:12:26 +0900

    Refactor : 몬스터 책임 분리
    Monter
    -> MonsterAI
    -> MonsterCombat
    -> MonsterMove
    -> MonsterStats

---
+++ Assets/02.Scripts/Monster/MonsterAI.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MonsterAI : MonoBehaviour
+{
+    [SerializeField] private EMonsterState _state = EMonsterState.Idle;
+    [SerializeField] private GameObject _player;
+
+    private MonsterMove _monsterMove;
+    private MonsterStats _stats;
+
+    private Vector3 _initPosition;
+    private float _distance;
+    private float _distanceToInit;
+    private Vector3 _direction;
+    private float _checkDistanceInterval = 0.2f;
+
+    public EMonsterState State => _state;
+
+    private void Awake()
+    {
+        _monsterMove = GetComponent<MonsterMove>();
+        _stats = GetComponent<MonsterStats>();
+        _initPosition = transform.position;
+    }
+    private void Start()
+    {
+        StartCoroutine(CheckDistance());
+    }
+    private void Update()
+    {
+        if (_monsterMove.IsKnockedBack) return;
+
+        HandleMonsterState();
+    }
+
+    public void SetState(EMonsterState newState)
+    {
+        _state = newState;
+    }
+
+    private void HandleMonsterState()
+    {
+        // 몬스터의 상태에 따라 다른 행동을 한다. (다른 메서드를 호출한다.)
+        switch (_state)
+        {
+            case EMonsterState.Idle:
+                Idle();
+                break;
+            case EMonsterState.Trace:
+                Trace();
+                break;
+            case EMonsterState.Comeback:
+                Comeback();
+                break;
+            case EMonsterState.Attack:
+                Attack();
+                break;
+            case EMonsterState.Hit:
+                break;
+            case EMonsterState.Death:
+                break;
+
+        }
+    }
+
+    private void Idle()
+    {
+        // 대기하는 상태
+        // TODO : Idle 애니메이션
+        if (_distance <= _stats.DetectDistance.Value)
+        {
+            _state = EMonsterState.Trace;
+        }
+    }
+
+    private void Trace()
+    {
+        // 플레이어를 쫓아가는 상태
+        // TODO : Run 애니메이션
+        _monsterMove.Move(_direction, _stats.MoveSpeed.Value);
+
+        if (_distance <= _stats.AttackDistance.Value)
+        {
+            _state = EMonsterState.Attack;
+        }
+
+        if (_distance > _stats.DetectDistance.Value)
+        {
+            _state = EMonsterState.Comeback;
+        }
+    }
+
+    private void Comeback()
+    {
+        if (_distanceToInit > 0.5f)
+        {
+            _monsterMove.Move(_direction, _stats.MoveSpeed.Value);
+        }
+        else
+        {
+            _state = EMonsterState.Idle;
+        }
+
+        if (_distance <= _stats.DetectDistance.Value)
+        {
+            _state = EMonsterState.Trace;
+        }
+    }
+
+    private void Attack()
+    {
+        if (_distance > _stats.AttackDistance.Value)
+        {
+            _state = EMonsterState.Trace;
+        }
+    }
+
+    private IEnumerator CheckDistance()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_checkDistanceInterval);
+
+            _distanceToInit = Vector3.Distance(transform.position, _initPosition);
+            _distance = Vector3.Distance(transform.position, _player.transform.position);
+            if (_state == EMonsterState.Comeback)
+            {
+                _direction = (_initPosition - transform.position).normalized;
+                continue;
+            }
+            _direction = (_player.transform.position - transform.position).normalized;
+        }
+    }
+}
---
+++ Assets/02.Scripts/Monster/MonsterCombat.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MonsterCombat : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _attackEffectVFX;
+    [SerializeField] private Transform _attackTransform;
+    [SerializeField] private Player _player;
+
+    private MonsterStats _stats;
+    private MonsterAI _ai;
+    private MonsterMove _move;
+    private float _attackTimer = 0f;
+
+    private void Awake()
+    {
+        _stats = GetComponent<MonsterStats>();
+        _ai = GetComponent<MonsterAI>();
+        _move = GetComponent<MonsterMove>();
+    }
+
+    private void Update()
+    {
+        _attackTimer += Time.deltaTime;
+
+        if (_ai.State == EMonsterState.Attack)
+        {
+            TryAttack();
+        }
+    }
+
+    private void TryAttack()
+    {
+        if (_attackTimer < _stats.AttackCoolTime.Value) return;
+
+        _attackTimer = 0f;
+        StartCoroutine(Attack_Coroutine());
+    }
+
+    private IEnumerator Attack_Coroutine()
+    {
+        Vector3 direction = (_player.transform.position - transform.position).normalized;
+
+        _attackEffectVFX.transform.position = _attackTransform.position;
+        _attackEffectVFX.transform.rotation = Quaternion.LookRotation(direction);
+        _attackEffectVFX.Play();
+        _player.TakeDamage(_stats.AttackDamage.Value);
+
+        yield return new WaitForSeconds(1f);
+    }
+
+    public bool TakeDamage(float damage)
+    {
+        if (_ai.State == EMonsterState.Hit || _ai.State == EMonsterState.Death)
+        {
+            return false;
+        }
+
+        if (_stats.Health.TryConsume(damage))
+        {
+            _ai.SetState(EMonsterState.Hit);
+            StartCoroutine(Hit_Coroutine());
+        }
+        else
+        {
+            _ai.SetState(EMonsterState.Death);
+            StartCoroutine(Death_Coroutine());
+        }
+        return true;
+    }
+
+    public void TakeKnockback(Vector3 direction, float amount)
+    {
+        _move.TakeKnockBack(direction, amount);
+    }
+
+    private IEnumerator Hit_Coroutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+        _ai.SetState(EMonsterState.Comeback);
+    }
+
+    private IEnumerator Death_Coroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+}

7c8dfca39cb20c78419868df60fde52e84d3860c
정종혁 <whdgur0068@gmail.com>
2025-12-15 16:46:07 +0900

    Refactor : 코드 리뷰 리팩토링

--- Assets/02.Scripts/UI/HUD/UI_PlayerHitScreen.cs
+++ Assets/02.Scripts/UI/HUD/UI_PlayerHitScreen.cs
@@ -4,16 +4,36 @@ using UnityEngine.UI;
 
 public class UI_PlayerHitScreen : MonoBehaviour
 {
+    [Header("Animation Settings")]
+    [SerializeField] private float _scaleDownDuration = 0.25f;
+    [SerializeField] private float _scaleUpDuration = 0.5f;
+    [SerializeField] private float _fadeDuration = 0.5f;
+    [SerializeField] private float _maxScale = 1.3f;
+
+    [Header("Components")]
     [SerializeField] private Image _hitScreen;
     [SerializeField] private Player _player;
-    private Sequence _hitSequence;
 
+    private Sequence _hitSequence;
+    private Transform _hitScreenTransform;
+
+    private void Awake()
+    {
+        _hitScreenTransform = _hitScreen.transform;
+    }
     private void Start()
     {
         _hitScreen.gameObject.SetActive(false);
         _player.OnHitPlayer += PlayerHit;
     }
-
+    private void OnDestroy()
+    {
+        if (_player != null)
+        {
+            _player.OnHitPlayer -= PlayerHit;
+        }
+        _hitSequence?.Kill();
+    }
     private void PlayerHit()
     {
         _hitScreen.gameObject.SetActive(true);
@@ -22,9 +42,9 @@ public class UI_PlayerHitScreen : MonoBehaviour
         _hitSequence?.Kill();
 
         _hitSequence = DOTween.Sequence();
-        _hitSequence.Append(_hitScreen.transform.DOScale(Vector3.one, 0.25f))
-                    .Append(_hitScreen.transform.DOScale(Vector3.one * 1.3f, 0.5f))
-                    .Join(_hitScreen.DOFade(0, 0.5f))
+        _hitSequence.Append(_hitScreenTransform.DOScale(Vector3.one, _scaleDownDuration))
+                    .Append(_hitScreenTransform.DOScale(Vector3.one * _maxScale, _scaleUpDuration))
+                    .Join(_hitScreen.DOFade(0, _fadeDuration))
                     .OnComplete(() => _hitScreen.gameObject.SetActive(false));
     }
 }
